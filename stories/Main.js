76aca3d39bd319e617d2d9469168bbff2a78b57e
Vitor José <vitorjsls30@gmail.com>
2019-03-15 16:48:17 -0300

    rf-01 - Adding first stories

--- stories/Main.js
+++ stories/Main.js
@@ -1,11 +1,54 @@
 import React from 'react';
-import Component from '../src/Main'; // This is our component
+import FullHeader from '../src/Main'; // This is our component
 import { storiesOf } from '@storybook/react';
 
-storiesOf('Component', module)
-    .add('with lyef name', () => (
-        <Component name="lyef" />
-    ))
-    .add('with another name', () => (
-        <Component name="another" />
-    ))
+storiesOf('FullHeader', module)
+  .add('with title', () => {
+    <FullHeader title="TDD" />
+  })
+  .add('with title and subtitle', () => {
+    <FullHeader 
+      title="TDD" 
+      subtitle="JS with TDD Course"/>
+  })
+  .add('with title, subtitle, bgColor', () => {
+    <FullHeader 
+      title="TDD" 
+      subtitle="JS with TDD Course"
+      bgColor="#3299BB"
+    />
+  })
+  .add('with title, subtitle, bgColor and textColor', () => {
+    <FullHeader 
+      title="TDD" 
+      subtitle="JS with TDD Course"
+      bgColor="#3299BB"
+      textColor="#FF9900"
+    />
+  })
+  .add('with title, subtitle, bgColor, textColor and font', () => {
+    <FullHeader 
+      title="TDD" 
+      subtitle="JS with TDD Course"
+      bgColor="#3299BB"
+      textColor="#FF9900"
+      font="cursive"
+    />
+  })
+  .add('with title, subtitle and bgImg', () => {
+    <FullHeader 
+      title="TDD" 
+      subtitle="JS with TDD Course"
+      bgImg="https://raw.githubusercontent.com/willianjusten/photo-examples/master/iceland-glacier.jpg"
+    />
+  })
+  .add('with title, subtitle and video', () => {
+    <FullHeader 
+      title="TDD" 
+      subtitle="JS with TDD Course"
+      bgColor="#EBE9EB"
+      textColor="#3299BB"
+      video="http://callmenick.com/_development/video/full-screen-background-video/media/demo.mp4"
+    />
+  })
+  
