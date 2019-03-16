b94ef6504505a990f9a754e0b839be44b1a26d56
Vitor José <vitorjsls30@gmail.com>
2019-03-16 16:54:09 -0300

    rfh-01 - Adding bgColor property

--- src/Main.js
+++ src/Main.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const FullHeader = ({ title, subtitle }) => (
-  <header>
+const defaultProps = {
+  bgColor: '#ccc',
+};
+
+const FullHeader = ({ title, subtitle, bgColor }) => {
+  const headerStyle = {
+    backgroundColor: bgColor,
+  };
+
+  return (<header style={headerStyle}>
     {title && <h1>{title}</h1>}
     {subtitle && <h2>{subtitle}</h2>}
-  </header>
-);
+  </header>);
+};
 
 const propTypes = {
   title: PropTypes.string,
   subtitle: PropTypes.string,
+  bgColor: PropTypes.string,
 };
 
 FullHeader.propTypes = propTypes;
+FullHeader.defaultProps = defaultProps;
 
 export default FullHeader;
