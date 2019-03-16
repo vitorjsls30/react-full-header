2ae124fb25b52b2636892346d82f59dd495ea7d4
Vitor José <vitorjsls30@gmail.com>
2019-03-16 13:32:40 -0300

    rfh-01 - Adding subtitle into the FulHeader component

--- tests/specs/Main.spec.js
+++ tests/specs/Main.spec.js
@@ -10,20 +10,38 @@ describe('<FullHeader />', () => {
     expect(wrapper.find('header')).to.have.length(1);
   });
 
-  it('should have h1 tag when title is passed', () => {
-    const wrapper = shallow(<FullHeader title="TDD" />);
-    expect(wrapper.find('h1')).to.have.length(1);
+  context('title', () => {
+    it('should have h1 tag when title is passed', () => {
+      const wrapper = shallow(<FullHeader title="TDD" />);
+      expect(wrapper.find('h1')).to.have.length(1);
+    });
+  
+    it('should not have h1 tag when title is not passed', () => {
+      const wrapper = shallow(<FullHeader />);
+      expect(wrapper.find('h1')).to.have.length(0);
+    });
+  
+    it('should have h1 tag with the title passed', () => {
+      const wrapper = shallow(<FullHeader title="TDD"/>);
+      expect(wrapper.find('h1').props().children).to.be.equal('TDD');
+    });
   });
 
-  it('should not have h1 tag when title is not passed', () => {
-    const wrapper = shallow(<FullHeader />);
-    expect(wrapper.find('h1')).to.have.length(0);
-  });
+  context('subtitle', () => {
+    it('should have h2 when subtitle passed', () => {
+      const wrapper = shallow(<FullHeader title="TDD" subtitle="Curso" />);
+      expect(wrapper.find('h2')).to.have.length(1);
+    });
 
-  it('should have h1 tag with the title passed', () => {
-    const wrapper = shallow(<FullHeader title="TDD"/>);
-    console.log('YO', wrapper.find('h1').props().children);
-    // expect(wrapper.find('h1')).to.have.length(0);
-  });
+    it('should not have h2 when subtitle is not passed', () => {
+      const wrapper = shallow(<FullHeader title="TDD" />);
+      expect(wrapper.find('h2')).to.have.length(0);
+    });
 
+    it('should have h2 tag with the subtitle passed', () => {
+      const wrapper = shallow(<FullHeader title="TDD" subtitle="Curso"/>);
+      expect(wrapper.find('h2').props().children).to.be.equal('Curso');
+    });
+
+  });
 });
